50a401a376b75109be3051514892c91b132594e4
claudiocldr <claudiocaldarella13@gmail.com>
2023-06-11 16:06:05 +0200

    The first controller - Postman, requests - exercise 4

--- demo/src/main/java/com/example/demo/randomApplication.java
+++ demo/src/main/java/com/example/demo/randomApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
 @SpringBootApplication
 @RestController
 public class randomApplication {
@@ -14,6 +18,17 @@ public class randomApplication {
     @GetMapping("/info")
     public ResponseEntity<String> info() {
         return ResponseEntity.status(200).build();
+    }
+
+    @GetMapping("/randomRequest")
+    public ResponseEntity<String> randomBadRequest() {
+        Random random = new Random();
+        boolean randomBoolean = random.nextBoolean();
+        if (randomBoolean) {
+            return ResponseEntity.status(400).body("ok");
+        } else {
+            return ResponseEntity.status(200).body("not ok");
+        }
     }
 
 }
