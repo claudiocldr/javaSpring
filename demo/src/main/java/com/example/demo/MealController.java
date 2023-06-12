e927066da105d863f3cdd4c997ac7bb386c5252a
claudiocldr <claudiocaldarella13@gmail.com>
2023-06-12 11:56:18 +0200

    Controller path variables, request params, returning objects - exercise 2

--- demo/src/main/java/com/example/demo/MealController.java
+++ demo/src/main/java/com/example/demo/MealController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
@@ -22,6 +24,13 @@ public class MealController {
     public ResponseEntity<List<Meal>> retrieveMeals() {
         return ResponseEntity.status(200).body(meals);
 
+    }
+
+    // mapping che ritorna una lista di meals dove ogni meal ha come nome la stringa passata come pathvariable
+    @GetMapping("/meal/{name}")
+    public ResponseEntity<List<Meal>> retrieveMealByName(@PathVariable String name) {
+        List<Meal> mealsRetrieved = meals.stream().filter(x -> x.getName().equals(name)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(mealsRetrieved);
     }
 }
 
